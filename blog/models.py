a4ba79c54358a77bbfc28c3ce5b315c5e24994f9
jurica29 <jrcpuljiz@gmail.com>
2022-06-15 19:28:12 +0000

    Add base html code across template files

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -10,5 +11,8 @@ class Post(models.Model):
     
     class Meta:
         ordering = ['-date_added']
+    
+    def __str__(self):
+        return self.title 
 
 
