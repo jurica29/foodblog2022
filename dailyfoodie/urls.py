a7dcb899c5d84d59f23922dd218b7df644d3cb66
jurica29 <jrcpuljiz@gmail.com>
2022-06-16 19:50:41 +0000

    Add functionality for post details page

--- dailyfoodie/urls.py
+++ dailyfoodie/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from blog.views import frontpage, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
+    path('', include('blogapp.urls')),
     path('', frontpage, name='frontpage'),
 ]
