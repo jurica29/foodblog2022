24abebe3199e5a78a78efbfd8e3d23ed35924e3d
jurica29 <jrcpuljiz@gmail.com>
2022-06-16 21:13:59 +0000

    Add category class for each post

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+    
+    # Displaying category names within django admin section
+    def __str__(self):
+        return self.title 
+
 # Create your models here.
 # Creating post class 
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
