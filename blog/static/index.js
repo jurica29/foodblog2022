198aeb08b0b8a91d2ba5fbc886c37a1c820b838c
jurica29 <jrcpuljiz@gmail.com>
2022-07-08 16:16:36 +0000

    Add comments for js and css code

--- blog/static/index.js
+++ blog/static/index.js
@@ -2,6 +2,7 @@
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
+//Adding event listener to cause the menu to show up when user clicks on three lines in the upper right corner
 "event.preventDefault()"
 burgerIcon.addEventListener("click", (event) => {
   navbarMenu.classList.toggle("is-active");
