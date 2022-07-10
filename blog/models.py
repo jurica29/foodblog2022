0c8129689c1a51bf086376b4589b963403808747
jurica29 <jrcpuljiz@gmail.com>
2022-07-10 19:52:47 +0000

    Add custom model for contact form

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
+# Custom model for contact form page consisting of below fields
+class Contact(models.Model):
+    fullname = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField(max_length=1000) 
+    date_created = models.DateField(verbose_name="Created on date", auto_now_add="True")
 
-# Create your models here.
+# Displaying email contact names within django admin section
+    def __str__(self):
+        return self.email
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,21 @@
 from django.http.response import HttpResponse
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# import forms in view
+from django import forms
+
+# used to send message
+from django.contrib import messages
+
+# import contact model
+from .models import Contact
+
+# create a form from model contact
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        exclude = ('date_created', )
 
 # Importing post model from models.py
 from blogapp.models import Post
@@ -11,9 +26,22 @@ def frontpage(request):
     posts = Post.objects.filter(status=Post.ACTIVE)
     return render(request, 'blog/frontpage.html', {'posts': posts})
 
-# About page view - renders "about" page
+# Contact page view 
 def about(request):
-    return render(request, 'blog/about.html')
+
+    #
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid:
+            form.save()
+            messages.success(request, 'You have successfully submitted your message!')
+
+    form = ContactForm
+
+    context = {'form':form}
+
+
+    return render(request, 'blog/about.html',context)
 
 # Robots_txt is used for dealing with bots
 def robots_txt(request):
@@ -22,3 +50,4 @@ def robots_txt(request):
         "Disallow: /admin/",
     ]
     return HttpResponse("\n".join(text), content_type="text/plain")
+
